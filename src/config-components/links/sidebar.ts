4ba6777ac8a7e818bc63884d9f4d4a186ffbb511
devHonorio <honorio.dev@gmail.com>
2024-02-08 14:29:20 -0300

    Configura links para sidebar

---
+++ src/config-components/links/sidebar.ts
@@ -0,0 +1,7 @@
+import { NavLinkProps } from '@/components/Layout/NavLink'
+
+export const configSidebarlinks: NavLinkProps[] = [
+  { href: '/dashboard', label: 'Dashboard' },
+  { href: '/categorias', label: 'Categorias' },
+  { href: '/produtos', label: 'Produtos' },
+]

a87dce721a5591790e6ca42627c9b80f69eeb1bb
devHonorio <honorio.dev@gmail.com>
2024-02-10 22:32:55 -0300

    Cria login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,23 +3,49 @@
 import Input from '@/components/comum/Input'
 import { Sform, Stitle } from './styles'
 import { Button } from '@/components/comum/Button'
-import { useRouter } from 'next/navigation'
+import zod from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useAuth } from '@/contexts/Authcontext'
 
 export default function Login() {
-  const router = useRouter()
+  const scheme = zod.object({
+    tel: zod
+      .string()
+      .min(11, 'O telefone deve conter DDD e o digito 9.')
+      .max(11, 'O telefone deve conter DDD e o digito 9.'),
+    password: zod.string().min(4, 'A sua senha deve ter pelo menos 4 caracteres.'),
+  })
 
-  const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    router.push('/dashboard')
+  type FormData = zod.infer<typeof scheme>
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(scheme),
+  })
+
+  const { signIn } = useAuth()
+
+  const handleLogin = async (data: FormData) => {
+    await signIn(data)
   }
   return (
     <>
-      <Sform onSubmit={handleLogin}>
+      <Sform onSubmit={handleSubmit(handleLogin)}>
         <Stitle className="mb-5">Login</Stitle>
 
-        <Input label="Telefone" />
+        <Input label="Telefone" {...register('tel')} error={errors.tel?.message} />
 
-        <Input label="Senha" type="password" typeof="numeric" />
+        <Input
+          label="Senha"
+          type="password"
+          typeof="numeric"
+          {...register('password')}
+          error={errors.password?.message}
+        />
 
         <Button className="w-full" type="submit">
           Entrar

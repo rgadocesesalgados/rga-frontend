49785b84c2fc8414871bcb7b392f73c224eda3f8
devHonorio <honorio.dev@gmail.com>
2024-02-12 01:10:50 -0300

    Ajusta caminho da rota categorias

--- src/config-components/links/sidebar.ts
+++ src/config-components/links/sidebar.ts
@@ -2,6 +2,6 @@ import { NavLinkProps } from '@/components/Layout/NavLink'
 
 export const configSidebarlinks: NavLinkProps[] = [
   { href: '/dashboard', label: 'Dashboard' },
-  { href: '/forms/categorias', label: 'Categorias' },
+  { href: '/categorias', label: 'Categorias' },
   { href: '/produtos', label: 'Produtos' },
 ]

e45ed1c76c98e3b48a7d00806c5cca9b9ccb9a7f
devHonorio <honorio.dev@gmail.com>
2024-02-12 01:11:53 -0300

    Cria alguns components para pensar em uma melhor organização

---
+++ src/app/categorias/page.tsx
@@ -0,0 +1,13 @@
+import Table from '@/components/comum/Table'
+import { Wrap } from '@/components/comum/Wrap'
+import Layout from '../dashboard/layout'
+
+export default function Categorias() {
+  return (
+    <Layout>
+      <Wrap>
+        <Table caption="Categorias" />
+      </Wrap>
+    </Layout>
+  )
+}
--- src/app/forms/categorias/page.tsx
+++ src/app/forms/categorias/page.tsx
@@ -1,8 +1,15 @@
+'use client'
+
 import { Sform } from '@/app/login/styles'
 import { Button } from '@/components/comum/Button'
 import Input from '@/components/comum/Input'
+import { useRouter } from 'next/navigation'
 
 export default function Categorias() {
+  const router = useRouter()
+  const cancel = () => {
+    router.push('/categorias')
+  }
   return (
     <div className="flex flex-col gap-5">
       <h1 className="text-xl">Categorias</h1>
@@ -11,7 +18,9 @@ export default function Categorias() {
         <Input label="Nome" />
 
         <div className="flex gap-5">
-          <Button color="outline">Cancelar</Button>
+          <Button color="outline" type="button" onClick={cancel}>
+            Cancelar
+          </Button>
           <Button className="flex-1">Salvar</Button>
         </div>
       </Sform>

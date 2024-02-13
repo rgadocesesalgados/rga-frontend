37d6c3a84b11014b40e068a64daafb82616711be
devHonorio <honorio.dev@gmail.com>
2024-02-13 15:51:29 -0300

    Cria component tabela

--- src/components/comum/Table/index.tsx
+++ src/components/comum/Table/index.tsx
@@ -1,56 +1,36 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
 import { Button } from '../Button'
-import { Scaption, Scontainer, Stable, Std, Sth } from './styles'
-import { PencilSquareIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Scaption, Scontainer, Stable, Sth } from './styles'
 
 export interface TableProps extends React.TableHTMLAttributes<HTMLTableElement> {
   caption: string
+  theads: string[]
+  onClickLabel?: string
+  onClick?: () => void
 }
 
-export default function Table({ caption }: TableProps) {
-  const router = useRouter()
-
-  const onClick = () => {
-    router.push('/forms/categorias')
-  }
+export default function Table({
+  caption,
+  onClickLabel = 'Adicionar',
+  theads,
+  children,
+  onClick,
+  ...props
+}: TableProps) {
   return (
     <Scontainer>
       <Button color="green" onClick={onClick}>
-        Adicionar nova categoria
+        {onClickLabel}
       </Button>
-      <Stable>
+      <Stable {...props}>
         <Scaption>{caption}</Scaption>
 
         <thead>
-          <tr>
-            <Sth>Nome</Sth>
-            <Sth>Produtos</Sth>
-            <Sth>Ações</Sth>
-          </tr>
+          <tr>{theads?.map((th) => <Sth key={th}>{th}</Sth>)}</tr>
         </thead>
 
-        <tbody className="divide-y">
-          <tr>
-            <Std>frios</Std>
-            <Std>3</Std>
-            <Std className="flex gap-5">
-              <PencilSquareIcon className="h-4 w-4" />
-              <XMarkIcon className="h-4 w-4" />
-            </Std>
-          </tr>
-
-          <tr>
-            <Std>fritos</Std>
-            <Std>12</Std>
-          </tr>
-
-          <tr>
-            <Std>doces tradicionais</Std>
-            <Std>5</Std>
-          </tr>
-        </tbody>
+        <tbody className="divide-y">{children}</tbody>
       </Stable>
     </Scontainer>
   )

066b5f7c9543845431b5afc87e8550f31c9f5819
devHonorio <honorio.dev@gmail.com>
2024-02-13 15:49:57 -0300

    cria rota para manipular categorias

--- src/app/categorias/page.tsx
+++ src/app/categorias/page.tsx
@@ -1,13 +1,79 @@
+'use client'
 import Table from '@/components/comum/Table'
 import { Wrap } from '@/components/comum/Wrap'
 import Layout from '../dashboard/layout'
+import { Std } from '@/components/comum/Table/styles'
+import { DeleteItem, EditItem } from '@/components/comum/Table/tableActions'
+import { CloseIcon, Pencil } from '@/components/icon'
+import Modal from '@/components/comum/Modal/Index'
+import { Sform } from '../login/styles'
+import Input from '@/components/comum/Input'
+import { Button } from '@/components/comum/Button'
+import { useHandleCategorias } from '@/hooks/categorias/useHandleCategoria'
+
+export interface CategoryProps {
+  name: string
+}
 
 export default function Categorias() {
+  const {
+    toogleModal,
+    data,
+    editItemModal,
+    deleteCategory,
+    isOpen,
+    setIsOpen,
+    handleSubmit,
+    submit,
+    register,
+    errors,
+    edit,
+    reset,
+  } = useHandleCategorias()
   return (
     <Layout>
       <Wrap>
-        <Table caption="Categorias" />
+        <Table caption="Categorias" theads={['Nome', 'Ações']} onClick={toogleModal}>
+          {!data.length && (
+            <tr>
+              <td>Cadastre uma categoria...</td>
+              <td></td>
+            </tr>
+          )}
+          {data?.map(({ name }) => (
+            <tr key={name}>
+              <Std>{name}</Std>
+              <Std className="flex gap-2">
+                <EditItem onClick={() => editItemModal(name)}>
+                  <Pencil strokeWidth={2} />
+                </EditItem>
+
+                <DeleteItem onClick={() => deleteCategory(name)}>
+                  <CloseIcon strokeWidth={3} />
+                </DeleteItem>
+              </Std>
+            </tr>
+          ))}
+        </Table>
       </Wrap>
+
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Categoria">
+        <Sform onSubmit={handleSubmit(submit)}>
+          <Input placeholder="Nome da categoria" {...register('name')} error={errors.name?.message} />
+          <div className="flex justify-end gap-5">
+            <Button
+              color="red"
+              onClick={() => {
+                setIsOpen(!isOpen)
+                reset()
+              }}
+            >
+              Cancelar
+            </Button>
+            {<Button>{edit ? 'Salvar alterações' : 'Cadastrar'}</Button>}
+          </div>
+        </Sform>
+      </Modal>
     </Layout>
   )
 }
--- src/app/forms/layout.tsx
+++
@@ -1,5 +0,0 @@
-import LayoutForm from '@/components/comum/LayoutForm'
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <LayoutForm>{children}</LayoutForm>
-}

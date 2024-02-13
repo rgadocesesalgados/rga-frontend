cf264a1eb1b4e19424a48c664be5af61bd3a7164
devHonorio <honorio.dev@gmail.com>
2024-02-13 15:51:46 -0300

    Cria estilos para icones

---
+++ src/components/icon/index.ts
@@ -0,0 +1,10 @@
+import { XMarkIcon, PencilSquareIcon } from '@heroicons/react/24/outline'
+import tw from 'tailwind-styled-components'
+
+export const CloseIcon = tw(XMarkIcon)`
+w-4 h-4
+`
+
+export const Pencil = tw(PencilSquareIcon)`
+w-4 h-4
+`

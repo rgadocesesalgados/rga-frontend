62023365b066bb51a70f31d4f9cb9ade4ff0cc8c
devHonorio <honorio.dev@gmail.com>
2024-05-19 17:25:17 -0300

    Relatorios de bolos

--- src/template/relatorios/bolos/columns.tsx
+++ src/template/relatorios/bolos/columns.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { Badge, BadgeProps } from '@/components/ui/badge'
 import { GetBolos } from '@/types/relatorios/bolos/get'
 import { ColumnDef } from '@tanstack/react-table'
 
@@ -24,6 +25,45 @@ export const columns: ColumnDef<GetBolos>[] = [
         .map((recheio) => recheio.name)
         .join(', '),
   },
+  {
+    accessorKey: 'cobertura',
+    header: 'Cobertura',
+    cell: ({ getValue }) => <span className="capitalize">{getValue<GetBolos['cobertura']>().toLocaleLowerCase()}</span>,
+  },
+  {
+    accessorKey: 'description',
+    header: 'Descrição',
+    cell: ({ cell }) => <span className="capitalize">{cell.getValue<string>() ? cell.getValue<string>() : '---'}</span>,
+  },
+  {
+    accessorKey: 'banner',
+    header: 'Modelo',
+    cell: ({ cell }) => {
+      const image = cell.getValue<string>()
+
+      return image ? (
+        <a href={image} target="_blank" rel="noreferrer">
+          <img className="rounded-2xl" alt={image} width={125} height={125} src={image} />
+        </a>
+      ) : (
+        '---'
+      )
+    },
+  },
+  {
+    accessorKey: 'topper',
+    header: 'Tem topper',
+    cell: ({ cell }) => (cell.getValue<GetBolos['topper']>() ? 'Sim' : 'Não'),
+  },
   { accessorKey: 'client', header: 'Cliente' },
   { accessorKey: 'hour', header: 'Horário' },
+  {
+    accessorKey: 'status_order',
+    header: 'Status do pedido',
+    cell: ({ getValue }) => (
+      <Badge variant={getValue<GetBolos['status_order']>().toLocaleLowerCase() as BadgeProps['variant']}>
+        {getValue<GetBolos['status_order']>()}
+      </Badge>
+    ),
+  },
 ]
--- src/types/relatorios/bolos/get.ts
+++ src/types/relatorios/bolos/get.ts
@@ -1,10 +1,23 @@
+import { GetOrder } from '@/types/order'
+
 export interface GetBolos {
   client: string
+  status_order: GetOrder['status']
   date: Date
   hour: string
   peso: number
   formato: 'QUADRADO' | 'REDONDO'
   massa: 'BRANCA' | 'CHOCOLATE' | 'MASSA_MESCLADA'
   recheio: { name: string }[]
+  cobertura: 'CHANTILLY' | 'AVELA_BATIDO' | 'NATA' | 'CLARA_QUEIMADA'
   description: string
+  banner: string
+  topper: {
+    name: string
+    description: string
+    banner: string
+    tema: string
+    idade: number
+    price: number
+  }
 }

2b718a4d4412bb094aa08c57bb50e45c92124103
devHonorio <honorio.dev@gmail.com>
2024-05-23 23:05:30 -0300

    Tabela de produtos

--- src/template/relatorios/produtos/Produtos.tsx
+++ src/template/relatorios/produtos/Produtos.tsx
@@ -1,6 +1,5 @@
-import { DataTable } from '@/components/ui-componets/data-table'
-import { columns } from './columns'
 import { GetRelatorio } from '@/types/relatorios/get'
+import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 
 export const Produtos = ({ data = [] }: { data: GetRelatorio['produtos'] }) => {
   const categorys = data.reduce((acc, item) => {
@@ -13,6 +12,34 @@ export const Produtos = ({ data = [] }: { data: GetRelatorio['produtos'] }) => {
   return categorys.map((category) => {
     const dataProdutos = data.filter((item) => item.category_name === category)
 
-    return <DataTable key={category} columns={columns} data={dataProdutos} inputFilter="name" inputFilterLabel="nome" />
+    return (
+      <div className="rounded-2xl border bg-white p-5" key={category}>
+        <p className="ml-2 text-lg font-bold">{category}</p>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Produto</TableHead>
+              <TableHead>Quantidade</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {dataProdutos.map((item) => (
+              <TableRow key={item.name}>
+                <TableCell>{item.name}</TableCell>
+                <TableCell>{item.quantity}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow className="bg-white">
+              <TableHead className="font-bold text-black">Total</TableHead>
+              <TableHead className="font-bold text-black">
+                {dataProdutos.reduce((acc, item) => acc + item.quantity, 0)}
+              </TableHead>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </div>
+    )
   })
 }
--- src/template/relatorios/produtos/columns.tsx
+++ src/template/relatorios/produtos/columns.tsx
@@ -5,4 +5,5 @@ import { ColumnDef } from '@tanstack/react-table'
 export const columns: ColumnDef<GetProdutos>[] = [
   { accessorKey: 'name', header: 'Produto' },
   { accessorKey: 'quantity', header: 'Quantidade' },
+  { id: 'actions', header: 'Total' },
 ]

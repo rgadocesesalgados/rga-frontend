892e459434dc4bb25bfcf8791423fc446fb8093a
devHonorio <honorio.dev@gmail.com>
2024-05-26 12:06:10 -0300

    adiciona filtro por status

--- src/contexts/relatorios/index.tsx
+++ src/contexts/relatorios/index.tsx
@@ -1,21 +1,27 @@
 'use client'
 
 import { api } from '@/services/api/apiClient'
+import { GetOrder } from '@/types/order'
 import { GetRelatorio } from '@/types/relatorios/get'
 import { createContext, useContext, useState } from 'react'
 
+export interface GetRelatoriosProps {
+  dateInicial: Date | null
+  dateFinal: Date | null
+  status: GetOrder['status'][]
+}
 interface RelatoriosContextData {
   relatorios: GetRelatorio
-  getRelatorios: (dateInicial?: Date, dateFinal?: Date) => Promise<void>
+  getRelatorios: (data: GetRelatoriosProps) => Promise<void>
 }
 const RelatoriosContext = createContext({} as RelatoriosContextData)
 
 export const ProviderRelatorios = ({ children }: { children: React.ReactNode }) => {
   const [relatorios, setRelatorios] = useState<GetRelatorio>()
 
-  const getRelatorios = async (dateInicial?: Date, dateFinal?: Date) => {
+  const getRelatorios = async ({ dateInicial, dateFinal, status }: GetRelatoriosProps) => {
     await api
-      .post('/relatorios', { dateInicial, dateFinal })
+      .post('/relatorios', { dateInicial, dateFinal, status })
       .then((response) => {
         setRelatorios(response.data)
         console.log({ relatorios: response.data })

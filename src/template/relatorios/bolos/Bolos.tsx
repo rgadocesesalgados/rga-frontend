4b0b2baea79a629f9224c37ec6ff70aa0c437f01
devHonorio <honorio.dev@gmail.com>
2024-05-26 12:04:53 -0300

    Muda posição do botão imprimir bolos

--- src/template/relatorios/bolos/Bolos.tsx
+++ src/template/relatorios/bolos/Bolos.tsx
@@ -1,7 +1,25 @@
 import { DataTable } from '@/components/ui-componets/data-table'
 import { columns } from './columns'
 import { GetRelatorio } from '@/types/relatorios/get'
+import { Button } from '@/components/ui/button'
+import { useModalPrint } from '@/contexts/modalPrint'
 
 export const Bolos = ({ data = [] }: { data: GetRelatorio['bolos'] }) => {
-  return <DataTable columns={columns} data={data} inputFilter="client" inputFilterLabel="cliente" />
+  const { handleOpen } = useModalPrint()
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex justify-between">
+        <h1 className="text-xl font-bold">Bolos</h1>
+        <Button
+          variant="outline"
+          onClick={() => {
+            handleOpen()
+          }}
+        >
+          Imprimir Bolos
+        </Button>
+      </div>
+      <DataTable columns={columns} data={data} inputFilter="client" inputFilterLabel="cliente" />
+    </div>
+  )
 }
